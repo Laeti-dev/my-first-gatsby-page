534192821443d53d0a877897e3302f426867181b
Laetitia Taddei <laetdi@gmail.com>
2022-03-19 16:00:38 +0900

    Tutorial part 3 - add a pluggin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 // Step 1: Import React
+import { StaticImage } from 'gatsby-plugin-image'
 import * as React from 'react'
 // Import the Link component from the Gatsby package and add a link to your About page
 // import { Link } from 'gatsby'
@@ -10,6 +11,10 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home'>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="sakura"
+        src="https://images.unsplash.com/photo-1647601160728-e29cd3cccd0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=1400&q=60"
+      />
     </Layout>
   )
 }
